50aa9726b1c4724b2f05be69b2b3a6fe3f322663
Javad <MrJavad.Email@gmail.com>
2026-02-10 10:54:37 +0330

    Edit(models): edit account model
    - add device_model, system_version, chat_id, is_permium and ....
    - path: src/database/models/account.py

--- src/database/models/account.py
+++ src/database/models/account.py
@@ -1,5 +1,5 @@
 from tortoise.models import Model
-from tortoise.fields import IntField, CharField, TextField
+from tortoise.fields import IntField, CharField, TextField, BooleanField
 
 
 class Account(Model):
@@ -7,7 +7,17 @@ class Account(Model):
     phone = CharField(max_length=20, unique=True, index=True)
     api_id = IntField()
     api_hash = CharField(max_length=100)
+    device_model = CharField(max_length=100)
+    system_version = CharField(max_length=100)
     session_string = TextField()
+
+    chat_id = IntField(max_length=15)
+    is_premium = BooleanField(default=False)
+    first_name = TextField()
+    last_name = TextField(null=True, default=None)
+    full_name = TextField()
+    username = CharField(max_length=20, null=True, default=None)
+    bio = TextField(null=True, default=None)
 
     class Meta:
         table = "accounts"
